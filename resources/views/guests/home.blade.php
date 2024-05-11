939686f1f64c9a7d3f8dae0956e899be2ec552cf
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-11 17:55:24 +0200

    style fix + add anchor tag button to index page

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>OOOOOOKAY</h1>
+    <div class="mx-auto mt-5 home-background">
+
+        <h1 class="text-info text-center">Select a date you would like to travel with Us!</h1>
+        <div class="button d-flex justify-content-center mt-5">
+
+            <button class="btn btn-dark border-warning bg-secondary px-3 py-1">
+                <a class="text-decoration-none text-warning fs-2" href="{{route('guests.index')}}">Trains for today</a>
+            </button>
+
+        </div>
+
+        
+
+    </div>
 @endsection
