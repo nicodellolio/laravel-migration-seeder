3c76807faaa78144b970e469c750a18adafd01a8
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-13 15:23:08 +0200

    seed database

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -17,17 +17,15 @@ class TrainsTableSeeder extends Seeder
 
         b:for ($i=0; $i < 15; $i++) { 
             $train = new Train();
-            $train->brand_company = $faker();
-            $train->departure_station = $faker();
-            $train->arrive_station = $faker();
-            $train->departure_time = $faker();
-            $train->arriving_time = $faker();
-            $train->train_code = $faker();
-            $train->wagon_count = $faker();
-            $train->on_time = $faker();
-            $train->canceled = $faker();
+            $train->brand_company = $faker->lastName() . 'Rail';
+            $train->departure_station = $faker->city();
+            $train->arrive_station = $faker->city();
+            $train->departure_time = $faker->time('H:i:s', '10:00:00');
+            $train->arriving_time = $faker->time('H:i:s', '23:59:59');
+            $train->train_code = $faker->randomNumber(5, true);
+            $train->wagon_count = $faker->numberBetween(9, 18);
+            $train->on_time = $faker->boolean();
             $train->save();
-
         }
 
     }
